be0373e9f4527e2a0a9b0b80771f47cf1057f287
Shobha Shinde <75949762+shobhashinde@users.noreply.github.com>
2025-03-10 11:49:22 +0530

    Phase 3.sql

--- Phase 3/Phase 3.sql
+++ Phase 3/Phase 3.sql
@@ -1,1 +1,11 @@
-P
+use bank;
+
+/* --------------------Bank Details ---------------------------------*/
+-- Drop the structure& records from the table
+drop table bank_details; 
+
+-- delete the records from table
+truncate table bank_details;
+
+-- 1.Retrive bank table records
+select * from bank_details;
